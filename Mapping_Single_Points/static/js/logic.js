30d5c13112095bcc1e9739e1a3405b8b22c3c401
tj1599 <tjsreekrisenjee@gmail.com>
2022-01-20 19:45:27 -0500

    adding updated single point

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,17 +1,13 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+
+//Create the map object with a center and zoom level
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
   radius: 300,
   color: "black",
-  fillColor: "ffffa1"
+  fillColor: "#ffffa1"
 }).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
